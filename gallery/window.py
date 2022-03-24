71ee7ad66f092f4fd0cdf63bf25eff53c52b59ba
Daniel Wood <s.d.wood.82@googlemail.com>
2022-03-24 08:42:56 +0000

    clean up

--- gallery/window.py
+++ gallery/window.py
@@ -39,17 +39,7 @@ class GalleryWindow(Gtk.ApplicationWindow):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.library = Library()
-        #self.store = Gio.ListStore()
-        #self.model = Gtk.SingleSelection.new(self.store)
-        #self.model = GtkDirectoryList()
-        #self.factory = Gtk.BuilderListItemFactory()
 
-        # GridView
-        # model -> GtK.SelectionModel
-        # factory -> Gtk.ListItemFactory
-
-        #self.model_items = Gio.ListStore()
         self.model_items = Gtk.DirectoryList()
         self.model_items.set_file(Gio.File.new_for_path('/home/sandal/Pictures/'))
         self.list_model = Gtk.SingleSelection(model=self.model_items)
@@ -63,73 +53,23 @@ class GalleryWindow(Gtk.ApplicationWindow):
 
         self.grid_layout.set_factory(self.factory)
 
-        self.load()
-
 
     def on_factory_setup(self, widget, item: Gtk.ListItem):
-        """ GtkSignalListItemFactory::setup signal callback
-        Setup the widgets to go into the ListView """
-
-        #print(widget, data)
-
         iw = Gtk.Image()
-        #iw.set_from_file('/home/sandal/Pictures/FeedsAndSpeed-Screenshot.png')
         iw.set_pixel_size(200)
         item.set_child(iw)
 
-
     def on_factory_bind(self, widget: Gtk.ListView, item: Gtk.ListItem):
-        """ GtkSignalListItemFactory::bind signal callback
-        apply data from model to widgets set in setup"""
-
-        #print(dir(widget), dir(item))
-        #print(item.get_data)
         data = item.get_item()
-        #print(data.get_attribute_object("standard::file"), "\n\n\n")
-        # fe = Gio.FileEnumerator
-        # fdata = fe.get_child(data)
-        # print(fdata)
-
         gfile = data.get_attribute_object("standard::file")
-        #print(gfile.get_path(), "\n\n\n")
-
-        #data.g_file_info_get_attribute_object(data, G_FILE_ATTRIBUTE_STANDARD_FILE)
-
         image = item.get_child()
         image.set_from_file(gfile.get_path())
-        #item.set_child(image)
 
     def on_factory_unbind(self, widget, item: Gtk.ListItem):
         pass
 
     def on_factory_teardown(self, widget, item: Gtk.ListItem):
         pass
-
-    def listbox_row_factory(self, imagePath) -> Gtk.Widget:
-            return ImageWidget(imagePath)
-
-    def add_to_model(self, path):
-        pass
-        #iw = image_widget.ImageWidget()
-        #iw.set_image(path)
-        #iw = Gtk.Image()
-        #iw.new_from_file(path)
-        #iw.set_pixel_size(200)
-        #str = Gtk.StringObject()
-        #str.set_string(path)
-        #self.model_items.append(str)
-
-
-    def load(self):
-
-        imgs = self.library.LoadImages()
-        #self.test_image.set_from_file(imgs[3])
-        for img in imgs:
-            self.add_to_model(img)
-
-
-
-
 
 class AboutDialog(Gtk.AboutDialog):
 
